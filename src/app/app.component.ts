a56172ea4c8e81fcf0082b356f5ab621ed0e8689
ArtemYazynin <yazynindev@gmail.com>
2019-10-07 18:45:33 +0400

    dynamic grid header/rows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,38 +10,42 @@ import { GridMetaData, Row, IndicatorRow, ColumnMetaData, ColumnSettings } from 
 })
 export class AppComponent implements OnInit {
   $gridMetaData: BehaviorSubject<GridMetaData>;
+
   ngOnInit(): void {
     this.$gridMetaData = this.$buildGridMetaData();
   }
   private $buildGridMetaData() {
     const rows = this.getRows();
-    const $metaData = this.$getColumnMetaData(rows)
-    const gridMetaData = new GridMetaData(new BehaviorSubject<Row[]>(rows), $metaData)
+    const $metaData = this.getColumnMetaData(rows);
+    const gridMetaData = new GridMetaData(rows, $metaData);
     return new BehaviorSubject<GridMetaData>(gridMetaData);
   }
 
   private getRows() {
     const rows: Row[] = [
-      new IndicatorRow(1, 'tip1', 'Типовой1', 11, 'fx1', 1.0079, 'H'),
-      new IndicatorRow(2, 'tip2', 'Типовой2', 11, 'fx2', 18.9984, 'H'),
-      new IndicatorRow(3, 'tip3', 'Типовой3', 11, 'fx3', 1.0079, 'H'),
+      new IndicatorRow(1, 'tip111111111122222211111111', 'Типовой1', 11, 'fx1', 1.0079, 'H'),
+      new IndicatorRow(2, 'tip2', 'Типовой2', 11, 'fx2', 18.9984, 'Y'),
+      new IndicatorRow(3, 'tip3', 'Типовой3', 11, 'fx3', 1.0079, 'm'),
       new IndicatorRow(4, 'tip4', 'Типовой4', 11, 'fx4', 6.941, 'H'),
-      new IndicatorRow(5, 'tip5', 'Типовой5', 11, 'fx5', 9.0122, 'H'),
-      new IndicatorRow(6, 'tip6', 'Типовой6', 11, 'fx6', 10.811, 'H'),
-      new IndicatorRow(7, 'tip7', 'Типовой7', 11, 'fx7', 12.0107, 'H'),
-      new IndicatorRow(8, 'tip8', 'Типовой8', 11, 'fx8', 14.0067, 'H'),
-      new IndicatorRow(9, 'tip9', 'Типовой9', 11, 'fx9', 15.9994, 'H'),
-      new IndicatorRow(10, 'tip10', 'Типовой10', 11, 'fx10', 20.1797, 'H'),
+      new IndicatorRow(5, 'tip5', 'Типовой5', 11, 'fx5', 9.0122, 'S'),
+      new IndicatorRow(6, 'tip6', 'Типовой6', 11, 'fx6', 10.811, 'P'),
+      new IndicatorRow(7, 'tip7', 'Типовой7', 11, 'fx7', 12.0107, 'K'),
+      new IndicatorRow(8, 'tip8', 'Типовой8', 11, 'fx8', 14.0067, 'Q'),
+      new IndicatorRow(9, 'tip9', 'Типовой9', 11, 'fx9', 15.9994, 'Z'),
+      new IndicatorRow(10, 'tip10', 'Типовой10', 11, 'fx10', 20.1797, 'A'),
     ]
     return rows;
   }
 
-  private $getColumnMetaData(rows: Row[]) {
-    let result = new ColumnMetaData();
-    rows.forEach((row, index) => {
-      const ifType1 = row.systemname === 'tip1';
-      result[row.systemname] = new ColumnSettings(ifType1, true);
-    });
-    return new BehaviorSubject<ColumnMetaData>(result);
+  private getColumnMetaData(rows: Row[]): ColumnMetaData {
+    const result = new ColumnMetaData();
+    result.id = new ColumnSettings(true, true, 'ID', 100);
+    result.systemname = new ColumnSettings(true, true, 'systemname', 100);
+    result.friendlyname = new ColumnSettings(false, true, 'friendlyname', 100);
+    result.code = new ColumnSettings(false, true, 'code', 100);
+    result.formula = new ColumnSettings(false, true, 'formula', 100);
+    result.weight = new ColumnSettings(false, true, 'weight', 100);
+    result.symbol = new ColumnSettings(false, true, 'symbol', 100);
+    return result;
   }
 }
--- src/app/modules/grid/models/grid-meta-data.model.ts
+++ src/app/modules/grid/models/grid-meta-data.model.ts
@@ -1,8 +1,29 @@
 import { BehaviorSubject } from 'rxjs';
 
 export class GridMetaData {
-    constructor(public dataSource: BehaviorSubject<Row[]>,
-        public columnMetaData: BehaviorSubject<ColumnMetaData>) {
+    $dataSource: BehaviorSubject<Row[]>;
+    $columnMetaData: BehaviorSubject<ColumnMetaData>;
+    $displayedColumns: BehaviorSubject<string[]>;
+    constructor(dataSource: Row[], columnMetaData: ColumnMetaData) {
+        this.$dataSource = new BehaviorSubject<Row[]>(dataSource);
+        this.$columnMetaData = new BehaviorSubject<ColumnMetaData>(columnMetaData);
+        this.initDisplayedColumns(columnMetaData);
+    }
+
+    private initDisplayedColumns(columnMetaData: ColumnMetaData) {
+        const displayedColumns = this.getVisibleColumns(columnMetaData);
+        this.$displayedColumns = new BehaviorSubject<string[]>(displayedColumns);
+    }
+
+    private getVisibleColumns(columnMetaData: ColumnMetaData): string[] {
+        const result = [];
+        if (!columnMetaData) { return result; }
+        for (const key in columnMetaData) {
+            if (columnMetaData.hasOwnProperty(key)) {
+                result.push(key);
+            }
+        }
+        return result;
     }
 }
 
@@ -12,10 +33,13 @@ export class ColumnMetaData {
 
 export class ColumnSettings {
     $isSticky: BehaviorSubject<boolean>;
-    $visible: BehaviorSubject<boolean>;
-    constructor(sticky: boolean, isVisible: boolean) {
+    $isVisible: BehaviorSubject<boolean>;
+    $width: BehaviorSubject<number>;
+    constructor(sticky: boolean, isVisible: boolean, public friendlyname: string,
+                width: number) {
         this.$isSticky = new BehaviorSubject<boolean>(sticky);
-        this.$visible = new BehaviorSubject<boolean>(isVisible);
+        this.$isVisible = new BehaviorSubject<boolean>(isVisible);
+        this.$width = new BehaviorSubject<number>(width);
     }
 }
 
@@ -35,9 +59,9 @@ export class Row {
 export class IndicatorRow extends Row {
     constructor(id: number, systemname: string, friendlyname: string,
         public code: number,
-        public formula,
+        public formula: string,
         public weight: number,
         public symbol: string) {
-        super(id, systemname, friendlyname)
+        super(id, systemname, friendlyname);
     }
 }
